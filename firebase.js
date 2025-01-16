1cf85b35040617f6809f2a1015e78f0b07b0ded0
Helena Penha <lenapenha02@hotmail.com>
2025-01-16 16:22:15 -0600

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_API_KEY,
+  authDomain: process.env.NEXT_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PROJECT_ID,
+  storageBucket: process.env.NEXT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
